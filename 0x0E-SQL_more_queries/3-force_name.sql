860cfa1fa7719da45e8dec218d9c068ebe7d03a8
Nonyfuego <Nonykuti@gmail.com>
2022-08-18 08:48:37 +0100

    ad ch

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,5 @@
--- Create table force_name
-CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
-
+-- creates the table force_name
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
