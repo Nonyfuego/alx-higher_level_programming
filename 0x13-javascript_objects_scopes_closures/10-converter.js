a15d9695626a3c125d09ae6eeb54a4c189cf2e16
Nony <99417300+Nonyfuego@users.noreply.github.com>
2022-12-14 13:11:24 +0100

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-// Converts a number from base 10 to another base
-exports.converter = function (base) {
-  return function (number) {
-    return number.toString(base);
-  };
-};
--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-// number of occurences of element in list
-exports.nbOccurences = function (list, element) {
-  return list.filter((x) => x === element).length;
-};
